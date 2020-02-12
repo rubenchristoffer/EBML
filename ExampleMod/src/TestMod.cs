083809913ee3ae0258e5780c95b953ef80067eea
Ruben Christoffer <rubenc@protonmail.com>
2020-02-12 16:43:51 +0100

    Added better description to mod content in ExampleMod

--- ExampleMod/src/TestMod.cs
+++ ExampleMod/src/TestMod.cs
@@ -37,16 +37,16 @@ namespace MyMod {
             );
 
             ModProperties.RegisterNewPropertyType(StructFactory.CreateStaticResourceBuildingsData(
-                "n1", "n2", "n3", "n4", "custom property", id, Property.Type.Production),
+                "Small uranium mine", "Medium uranium mine", "Large uranium mine", "Huge uranium mine", "Modded property", id, Property.Type.Production),
                 ModFiles.CreateSprite(ModFiles.CreateTexture(ModFiles.ReadFileFromDisk(@"ExampleMod\atombomb.png"))));
         }
 
         public override void OnPostInit() {
-            Singletons.PROPERTY_CONTROLLER.GetStaticResourceBuildings().staticResourceBuildingsDataArr.ToList()
+            /*Singletons.PROPERTY_CONTROLLER.GetStaticResourceBuildings().staticResourceBuildingsDataArr.ToList()
                 .ForEach(srb => ModLoader.Log(string.Format("id: {0}, n1: {1}, n2: {2}, n3: {3}, n4: {4}, des: {5}, " +
                 "res_id: {6}, income_bonus: {7}, building_type: {8}, id_lvl1: {9}, id_lvl2: {10}, id_lvl3: {11}, id_lvl4: {12}\n", srb.id, Singletons.LOCALIZATION_CONTROLLER.GetText(srb.name1), Singletons.LOCALIZATION_CONTROLLER.GetText(srb.name2), Singletons.LOCALIZATION_CONTROLLER.GetText(srb.name3), Singletons.LOCALIZATION_CONTROLLER.GetText(srb.name4),
                 Singletons.LOCALIZATION_CONTROLLER.GetText(srb.des), srb.resourse_id, srb.income_bonus, (UserProperty.Type) srb.building_type, srb.id_ico_lvl1, srb.id_ico_lvl2, srb.id_ico_lvl3,
-                srb.id_ico_lvl4)));
+                srb.id_ico_lvl4)));*/
         }
 
     }
